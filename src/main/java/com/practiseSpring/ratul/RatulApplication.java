2f45f014a20ab609f314c714fcfe234e31ae173d
ratul <ratulde1990@icloud.com>
2024-09-21 16:13:22 +0530

    Initial commit

---
+++ src/main/java/com/practiseSpring/ratul/RatulApplication.java
@@ -0,0 +1,13 @@
+package com.practiseSpring.ratul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RatulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RatulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/practiseSpring/ratul/RatulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practiseSpring.ratul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RatulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
