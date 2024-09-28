6e5b8afdb2d6ac9d998fa00d3defbfc2e666d6cc
ratul <ratulde1990@icloud.com>
2024-09-28 21:49:11 +0530

    Code changes working

--- src/main/java/DAO/PersonDAOImpl.java
+++
@@ -1,7 +0,0 @@
-package DAO;
-
-//import org.springframework.data.repository.CrudRepository;
-
-public interface PersonDAOImpl  {
-    //extends CrudRepository<PersonObject, String>
-}
---
+++ src/main/java/com/practiseSpring/ratul/DAO/PersonObject.java
@@ -0,0 +1,32 @@
+package com.practiseSpring.ratul.DAO;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Past;
+import lombok.*;
+import jakarta.persistence.*;
+
+import java.util.Date;
+
+@Getter
+@Setter
+@ToString
+@Entity
+@NoArgsConstructor
+@Table (name="Person")
+public class PersonObject {
+
+    @Id
+    @NotNull
+    @Column(name="USER_NAME")
+    private String name;
+
+    @NotNull
+    @Past
+    @Column(name="DATE_OF_BIRTH")
+    private Date dateOfBirth;
+
+    public PersonObject(String name, Date dateOfBirth) {
+        super();
+        this.name = name;
+        this.dateOfBirth = dateOfBirth;
+    }
+}
